1a99c805d6d041ab3bc832e476a21aaf359d1600
PabloMilletRios <pablomill96@gmail.com>
2024-12-04 09:42:54 +0100

    filtros_api_js end

--- client/src/helper/promiseHelper.js
+++ client/src/helper/promiseHelper.js
@@ -2,7 +2,7 @@ import { News } from "../classes/News.js";
 
 export function parseClases(Noticias_array) {
     return new Promise((resolve,reject)=>{
-       if (Noticias_array !== null) {
+       if (Noticias_array !== null || Noticias_array !== undefined) {
         const newClasses = Noticias_array.map((Noticias)=>{
             const Noticias_Parse = new News(Noticias.title,Noticias.url,Noticias.source);
             return Noticias_Parse;
